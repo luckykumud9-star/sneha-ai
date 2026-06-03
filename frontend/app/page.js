a0bf7b3d5c1d83c3b3c7bcd510a00d33ed26beae
luckykumud9-star <luckykumud9@gmail.com>
2026-06-03 12:32:12 +0530

    Create page.js

---
+++ frontend/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Sneha AI ❤️</h1>
+
+      <p>
+        Welcome Yash
+      </p>
+
+      <button>
+        Start Chat
+      </button>
+    </main>
+  );
+}
